cf645573da4e5990cddadec92d35daffcf3bdec5
yanhaha233 <541996081@qq.com>
2022-06-23 22:53:11 +0800

    0623

--- docs/sw.js
+++ docs/sw.js
@@ -2,8 +2,8 @@ var cacheName = 'egui-template-pwa';
 var filesToCache = [
   './',
   './index.html',
-  './eframe_template.js',
-  './eframe_template_bg.wasm',
+  './yanhaha_gui.js',
+  './yanhaha_gui_bg.wasm',
 ];
 
 /* Start the service worker and cache all of the app's content */
--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,8 @@ fn main() {
 
     let native_options = eframe::NativeOptions::default();
     eframe::run_native(
-        "eframe template",
+        "yanhaha_gui",
         native_options,
-        Box::new(|cc| Box::new(eframe_template::TemplateApp::new(cc))),
+        Box::new(|cc| Box::new(yanhaha_gui::TemplateApp::new(cc))),
     );
 }
